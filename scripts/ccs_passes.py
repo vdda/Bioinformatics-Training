0da402e54d7f8eb05cf6957f2c0fa36111527420
Lawrence Hon <lhon@pacificbiosciences.com>
2015-05-29 16:52:21 -0700

    updated to handle multiple parts per movie 	modified:   ccs_passes.py

--- scripts/ccs_passes.py
+++ scripts/ccs_passes.py
@@ -2,8 +2,12 @@ import os, h5py
 
 def get_ccs_passes(filename):
     hf = h5py.File(filename)
-    basename = os.path.basename(filename).replace('.ccs.h5', '')
-    for hole_num, num_passes in enumerate(hf['/PulseData/ConsensusBaseCalls/Passes/NumPasses'].value):
+    basename = os.path.basename(filename)[:-9]
+
+    suffix = filename[-9:]
+    assert suffix in ('.1.ccs.h5', '.2.ccs.h5', '.3.ccs.h5')
+
+    for hole_num, num_passes in zip(hf['/PulseData/ConsensusBaseCalls/ZMW/HoleNumber'].value, hf['/PulseData/ConsensusBaseCalls/Passes/NumPasses'].value):
         if num_passes > 0:
             print '%s/%d/ccs' % (basename, hole_num), num_passes
 
